1c59501ec3b3b5a89652c25b69dc296ddebd2b15
GiselleBarbosa <g88barbosa@gmail.com>
2024-04-01 13:31:37 -0300

    first commit

---
+++ src/main/java/com/giselle/loginauthapi/LoginAuthApiApplication.java
@@ -0,0 +1,12 @@
+package com.giselle.loginauthapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginAuthApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginAuthApiApplication.class, args);
+	}
+}
